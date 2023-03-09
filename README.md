63385339b18036191a7fb505fb0d724b98d435bf
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-03-09 13:43:48 +0100

    Home screen complete

---
+++ README.md
@@ -0,0 +1,53 @@
+# GoTravel - A React Native travel app
+
+> Explore attractions, hotels, and restaurants in your city or any city in the world.
+
+## Description
+
+This is a mobile app that allows users to explore attractions, hotels, and restaurants in their city or any city in the world using Google places API.
+
+## Instructions:
+
+### Cloning the repo to your local system (if you already have git, installed in your system)
+
+1.  Copy this Link `https://github.com/Hombre2014/GoTravel.git`
+2.  Open your terminal or command line
+3.  Run the command: `git clone https://github.com/Hombre2014/GoTravel.git`
+4.  Open the folder with your code editor: `cd GoTravel`
+5.  Run `yarn start` to start the server
+6.  Scan the QR code with your phone or run the app in an emulator
+
+## Built With
+
+- Major languages: JavaScript
+- Frameworks: React Native, Tailwind CSS
+
+## Live Demo
+
+- [Live demo link]()
+
+## Deployment
+
+- All the files necessary for deployment are in the /dist folder
+
+## Author
+
+👤 **Yuriy Chamkoriyski**
+
+- GitHub: [@Hombre2014](https://github.com/Hombre2014)
+- Twitter: [@Chamkoriyski](https://twitter.com/Chamkoriyski)
+- LinkedIn: [axebit](https://linkedin.com/in/axebit)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/Hombre2014/GoTravel.git/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./license.md) licensed.
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { HeroImage } from '../assets';
+import * as Animatable from 'react-native-animatable';
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text className="text-red-400">HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="white flex-1 relative">
+      {/* First Section */}
+      <View className="flex-row px-6 mt-2 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00bcc9] text-3xl font-semibold">Go</Text>
+        </View>
+        <Text className="text-[#2a2b4b] text-3xl font-semibold">Travel</Text>
+      </View>
+
+      {/* Second Section */}
+      <View className="px-6 mt-2 space-y-1">
+        <Text className="text-[#3c6072] text-[42px]">Enjoy the trip with</Text>
+        <Text className="text-[#00bcc9] text-[38px] font-bold">Good moments</Text>
+        <Text className="text-[#3c6072] text-sm">
+          Find attractions, hotels and restaurants in any place around the World, the easiest way!
+        </Text>
+      </View>
+
+      {/* Circle Section */}
+      <View className="w-[320px] h-[320px] bg-[#00bcc9] rounded-full absolute top-96 -right-36"></View>
+      <View className="w-[320px] h-[320px] bg-[#e99265] rounded-full absolute -bottom-16 -left-28"></View>
+
+      {/* Hero Section */}
+      <View className="flex-1 relative items-center justify-center">
+        <Animatable.Image
+          animation="fadeIn"
+          easing="ease-in-out"
+          source={HeroImage}
+          className="w-full h-full object-cover mt-10"
+        />
+        <View className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 rounded-full border-[#00bcc9] items-center justify-center">
+          <TouchableOpacity>
+            <Animatable.View animation={"pulse"} easing="ease-in-out" iterationCount={"infinite"} className="w-20 h-20 items-center justify-center rounded-full bg-[#00bcc9]">
+              <Text className="text-gray-50 text-[36px] font-semibold">Go</Text>
+            </Animatable.View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default HomeScreen;
