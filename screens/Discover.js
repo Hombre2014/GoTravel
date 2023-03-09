691a63ce9488ac68dc06698cd1952e7081258bc8
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-03-09 17:40:32 +0100

    Completed the Discover screen menu items

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,11 +1,81 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, Image, ScrollView } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import React, { useLayoutEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Avatar, Hotels, Attractions, Restaurants } from '../assets';
+import { API_KEY } from '@env';
+import MenuContainer from '../components/MenuContainer';
 
 const Discover = () => {
+
+  const navigation = useNavigation();
+
+  const [type, setType] = useState("restaurants");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Discover</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row items-center justify-between px-8">
+        <View>
+          <Text className="text-[40px] text-[#0b646b] font-bold">Discover</Text>
+          <Text className="text-[#527273] text-[32px]">the beauty today</Text>
+        </View>
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center" style={{ elevation: 12 }}>
+          <Image
+            source={Avatar}
+            className="w-full h-full rounded-md object-cover"
+          />
+        </View>
+      </View>
+      <View className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 mt-4" style={{ elevation: 16 }}>
+        <GooglePlacesAutocomplete
+          GooglePlacesDetailsQuery={{ fields: 'geometry' }}
+          placeholder='Search'
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(details?.geometry?.viewport);
+          }}
+          query={{
+            key: API_KEY,
+            language: 'en',
+          }}
+        />
+      </View>
+
+      {/* Menu Container */}
+      <ScrollView>
+        <View className="flex-row items-center justify-between px-8 mt-8">
+          <MenuContainer
+            key={"hotel"}
+            title="Hotels"
+            imageSrc={Hotels}
+            type={type}
+            setType={setType}
+          />
+          <MenuContainer
+            key={"attractions"}
+            title="Attractions"
+            imageSrc={Attractions}
+            type={type}
+            setType={setType}
+          />
+          <MenuContainer
+            key={"restaurants"}
+            title="Restaurants"
+            imageSrc={Restaurants}
+            type={type}
+            setType={setType}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
