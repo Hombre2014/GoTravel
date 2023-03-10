259dd84de6a8655ed66cebd9ca44ba0c3e5c63f5
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-03-10 20:28:48 +0100

    App is complete

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,28 +1,33 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
 import {
   View,
   Text,
+  SafeAreaView,
   Image,
   ScrollView,
-  ActivityIndicator,
   TouchableOpacity,
-} from 'react-native';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import React, { useLayoutEffect, useState, useEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { Avatar, Hotels, Attractions, Restaurants, NotFound } from '../assets';
-import { API_KEY } from '@env';
-import MenuContainer from '../components/MenuContainer';
-import { FontAwesome } from '@expo/vector-icons';
-import ItemCardContainer from '../components/ItemCardContainer';
+  ActivityIndicator,
+} from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useNavigation } from "@react-navigation/native";
+import { Attractions, Avatar, Hotels, NotFound, Restaurants } from "../assets";
+import MenuContainer from "../components/MenuContainer";
+
+import { FontAwesome } from "@expo/vector-icons";
+import ItemCardContainer from "../components/ItemCardContainer";
 import { getPlacesData } from "../api";
+import { API_KEY } from "@env";
 
 const Discover = () => {
   const navigation = useNavigation();
 
-  const [type, setType] = useState('restaurants');
+  const [type, setType] = useState("restaurants");
   const [isLoading, setIsLoading] = useState(false);
   const [mainData, setMainData] = useState([]);
+  const [bl_lat, setBl_lat] = useState(null);
+  const [bl_lng, setBl_lng] = useState(null);
+  const [tr_lat, setTr_lat] = useState(null);
+  const [tr_lng, setTr_lng] = useState(null);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -32,74 +37,75 @@ const Discover = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    getPlacesData().then((data) => {
+    getPlacesData(bl_lat, bl_lng, tr_lat, tr_lng, type).then((data) => {
       setMainData(data);
       setInterval(() => {
         setIsLoading(false);
       }, 2000);
     });
-  }, []);
+  }, [bl_lat, bl_lng, tr_lat, tr_lng, type]);
 
   return (
     <SafeAreaView className="flex-1 bg-white relative">
-      <View className="flex-row items-center justify-between px-8">
+      <View className="flex-row items-center justify-between px-8 mt-6">
         <View>
-          <Text className="text-[40px] text-[#0b646b] font-bold">Discover</Text>
-          <Text className="text-[#527273] text-[32px]">the beauty today</Text>
+          <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527283] text-[36px]">the beauty today</Text>
         </View>
-        <View
-          className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center"
-          style={{ elevation: 12 }}
-        >
+
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-lg">
           <Image
             source={Avatar}
             className="w-full h-full rounded-md object-cover"
           />
         </View>
       </View>
-      <View
-        className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 mt-4"
-        style={{ elevation: 16 }}
-      >
+
+      <View className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 shadow-lg mt-4 border" style={{ elevation: 12 }}>
         <GooglePlacesAutocomplete
-          GooglePlacesDetailsQuery={{ fields: 'geometry' }}
+          GooglePlacesDetailsQuery={{ fields: "geometry" }}
           placeholder="Search"
           fetchDetails={true}
           onPress={(data, details = null) => {
             // 'details' is provided when fetchDetails = true
-            console.log(details?.geometry?.viewport);
+            setBl_lat(details?.geometry?.viewport?.southwest?.lat);
+            setBl_lng(details?.geometry?.viewport?.southwest?.lng);
+            setTr_lat(details?.geometry?.viewport?.northeast?.lat);
+            setTr_lng(details?.geometry?.viewport?.northeast?.lng);
           }}
           query={{
             key: API_KEY,
-            language: 'en',
+            language: "en",
           }}
         />
       </View>
 
       {/* Menu Container */}
       {isLoading ? (
-        <View className="flex-1 items-center justify-center">
-          <ActivityIndicator size="large" color="#ob646b" />
+        <View className=" flex-1 items-center justify-center">
+          <ActivityIndicator size="large" color="#0B646B" />
         </View>
       ) : (
         <ScrollView>
-          <View className="flex-row items-center justify-between px-8 mt-8">
+          <View className=" flex-row items-center justify-between px-8 mt-8">
             <MenuContainer
-              key={'hotel'}
+              key={"hotels"}
               title="Hotels"
               imageSrc={Hotels}
               type={type}
               setType={setType}
             />
+
             <MenuContainer
-              key={'attractions'}
+              key={"attractions"}
               title="Attractions"
               imageSrc={Attractions}
               type={type}
               setType={setType}
             />
+
             <MenuContainer
-              key={'restaurants'}
+              key={"restaurants"}
               title="Restaurants"
               imageSrc={Restaurants}
               type={type}
@@ -107,53 +113,49 @@ const Discover = () => {
             />
           </View>
 
-          {/* List of places */}
           <View>
-            <View className="flex-row items-center justify-between px-8 mt-8">
-              <Text className="text-[#2c7379] text-[28px] font-bold">
+            <View className="flex-row items-center justify-between px-4 mt-8">
+              <Text className="text-[#2C7379] text-[28px] font-bold">
                 Top Tips
               </Text>
               <TouchableOpacity className="flex-row items-center justify-center space-x-2">
-                <Text className="text-[#a0c4c7] text-[20px] font-bold">
+                <Text className="text-[#A0C4C7] text-[20px] font-bold">
                   Explore
                 </Text>
                 <FontAwesome
                   name="long-arrow-right"
                   size={24}
-                  color="#a0c4c7"
+                  color="#A0C4C7"
                 />
               </TouchableOpacity>
             </View>
 
-            <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
+            <View className="px-2 mt-8 flex-row items-center justify-evenly flex-wrap">
               {mainData?.length > 0 ? (
                 <>
-                  <ItemCardContainer
-                    key={'101'}
-                    imageSrc={
-                      'https://cdn.pixabay.com/photo/2023/03/02/03/01/bird-7824442__340.jpg'
-                    }
-                    title="Something longer then"
-                    location="Doha extra log location"
-                  />
-                  <ItemCardContainer
-                    key={'102'}
-                    imageSrc={
-                      'https://cdn.pixabay.com/photo/2023/03/03/17/36/peafowl-7828140__340.jpg'
-                    }
-                    title="Sample"
-                    location="Katar"
-                  />
+                  {mainData?.map((data, i) => (
+                    <ItemCardContainer
+                      key={i}
+                      imageSrc={
+                        data?.photo?.images?.medium?.url
+                          ? data?.photo?.images?.medium?.url
+                          : "https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"
+                      }
+                      title={data?.name}
+                      location={data?.location_string}
+                      data={data}
+                    />
+                  ))}
                 </>
               ) : (
                 <>
-                  <View className="w-full h-[300px] items-center space-y-8 justify-center">
+                  <View className="w-full h-[400px] items-center space-y-8 justify-center">
                     <Image
                       source={NotFound}
-                      className="w-24 h-24 object-cover"
+                      className=" w-32 h-32 object-cover"
                     />
-                    <Text className="text-[#428288] text-2xl font-semibold">
-                      Opps... No results found
+                    <Text className="text-2xl text-[#428288] font-semibold">
+                      Opps...No Data Found
                     </Text>
                   </View>
                 </>
